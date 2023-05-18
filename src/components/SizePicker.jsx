a6705132a8b5c03f7286bb386a7eaa1ec7e9ade9
Swayze <theartintheraw@gmail.com>
2023-05-18 19:02:54 -0400

    sizepicker fixed

--- src/components/SizePicker.jsx
+++ src/components/SizePicker.jsx
@@ -1,29 +1,29 @@
-import React, {useState} from 'react';
-import {useSnapshot} from 'valtio';
+import React, { useState } from 'react'
+import { useSnapshot } from 'valtio'
 
-import state from '../store';
+import state from '../store'
 
-const SizePicker = size => {
-	const snap = useSnapshot(state);
-	const [selectedSize, setSelectedSize] = useState(snap.size);
+const SizePicker = (size) => {
+  const snap = useSnapshot(state)
+  const [selectedSize, setSelectedSize] = useState(snap.size)
 
-	size = size || snap.size;
+  size = size || snap.size
 
-	const handleChange = e => {
-		setSelectedSize(e.state.size);
-	};
+  const handleChange = (e) => {
+    setSelectedSize(e.state.size)
+  }
 
-	return (
-		<div className='absolute left-full ml-3'>
-			<select className='sizepicker-container' value={selectedSize}>
-				{snap.sizes.map(size => (
-					<option key={size} onChange={handleChange} onClick={() => (state.size = size)}>
-						{size}
-					</option>
-				))}
-			</select>
-		</div>
-	);
-};
+  return (
+    <div className="absolute left-full ml-3">
+      <select className="sizepicker-container">
+        {snap.sizes.map((size) => (
+          <option key={size} onChange={handleChange} onClick={() => (state.size = size)}>
+            {size}
+          </option>
+        ))}
+      </select>
+    </div>
+  )
+}
 
-export default SizePicker;
+export default SizePicker
